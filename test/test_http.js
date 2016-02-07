76b9f4637f07d6b8a79b64fa1cb6b4a7562af7bf
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-07 11:54:31 -0800

    Add more http helpers
    With tests, cause otherwise I would botch them

---
+++ test/test_http.js
@@ -0,0 +1,45 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+
+const assert = require('assert');
+const Helpers = require('../lib/helpers');
+
+// test http helpers using some of the best nanoservices on the web
+
+function testGet() {
+    Helpers.Http.get('http://www.foaas.com/because/Me', { accept: 'application/json' }).then(function(data) {
+        var parsed = JSON.parse(data);
+        assert.equal(parsed.message, 'Why? Because Fuck you, that\'s why.');
+        assert.equal(parsed.subtitle, '- Me');
+    }).catch(function(e) {
+        console.error('*** testGet: ' + e.message);
+        console.error(e.stack);
+    }).done();
+}
+
+function testPost() {
+    Helpers.Http.post('http://api.shoutcloud.io/V1/SHOUT',
+                      JSON.stringify({ INPUT: 'all caps' }),
+                      { accept: 'application/json',
+                        dataContentType: 'application/json' })
+        .then(function(data) {
+            var parsed = JSON.parse(data);
+            assert.equal(parsed.INPUT, 'all caps');
+            assert.equal(parsed.OUTPUT, 'ALL CAPS');
+        }).catch(function(e) {
+            console.error('*** testPost: ' + e.message);
+            console.error(e.stack);
+        }).done();
+}
+
+function main() {
+    testGet();
+    testPost();
+}
+
+main();
