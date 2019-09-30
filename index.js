5a65df35f99c2afceb48673c82b1567361fd3b12
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-09-30 13:44:51 -0700

    Add infrastructure to generate jsdoc
    And a few jsdocs to see how they look like.

--- index.js
+++ index.js
@@ -26,22 +26,59 @@ const BasePlatform = require('./lib/base_platform');
 
 const ThingTalk = require('thingtalk');
 
+/**
+ * Versioning information for the library.
+ *
+ * Use this object to check if the currently loaded Thingpedia SDK
+ * is compatible with your device, and to dynamically check if a specific
+ * feature is present.
+ *
+ * Note: you should never bundle the Thingpedia SDK with your device.
+ *
+ * @alias version
+ * @namespace
+ */
 const VERSION = {
+    /** Major version number (incremented on incompatible changes) */
     major: 2,
+    /** Minor version number (incremented on feature additions) */
     minor: 6,
+    /** Full version string, in semantic version format */
     full: '2.6.0-alpha.1',
+    /** Convert the version number to a number (for comparisons) */
     valueOf() {
         return this.major * 100 + this.minor;
     },
     toString() {
         return this.full;
     },
+    /**
+     * Check if the current version is compatible with the passed in version
+     *
+     * @param {number|Version} v - the version, as a number or object with `major`
+     *                             and minor properties
+     * @return {Boolean}
+     */
     compatible(v) {
         if (typeof v === 'number')
             return this.valueOf() >= v && Math.floor(v/100) === this.major;
         else
             return this.major === v.major && this.minor >= v.minor;
     },
+    /**
+     * Check if the given feature is present.
+     *
+     * It is ok to pass invalid or unrecognized feature names to this function,
+     * which will then return `false`.
+     *
+     * In this version of the library, the following feature names are recognized:
+     * - `rss`: RSS helpers and loaders
+     * - `value-types`: ThingTalk value types are re-exported
+     * - `thingpedia-client`: Thingpedia Client APIs are present
+     *
+     * @param {String} f - a feature name
+     * @return {Boolean} whether the named feature is supported.
+     */
     hasFeature(f) {
         switch (f) {
         case 'rss':
