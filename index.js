26a42f407520d5311d164739a01c2abcbc1ac790
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-08-26 08:50:04 -0700

    v2.8.0-beta.3

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const VERSION = {
     /** Minor version number (incremented on feature additions) */
     minor: 8,
     /** Full version string, in semantic version format */
-    full: '2.8.0-beta.2',
+    full: '2.8.0-beta.3',
     /** Convert the version number to a number (for comparisons) */
     valueOf() {
         return this.major * 100 + this.minor;
