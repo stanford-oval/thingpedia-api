988152f999b473b2e767185465f9a6a286ef4baf
Jake Wu <jmhw0123@gmail.com>
2022-12-01 13:53:54 +0800

    modified updateModule

--- lib/downloader.ts
+++ lib/downloader.ts
@@ -137,7 +137,7 @@ export default class ModuleDownloader {
                 // keep the old module we had already loaded
                 // this avoids reloading the JS code multiple times
                 // unnecessarily
-                this._moduleRequests.set(id, Promise.resolve(oldModule));
+                this._moduleRequests.set(id, Promise.resolve(newModule));
             } else {
                 // remove any remnant of the old module
                 await oldModule.clearCache();
